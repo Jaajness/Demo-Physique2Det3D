4d9511ed41cde5ff37369e967c60624f14fb85a0
Jaajness <jaajnessmain@gmail.com>
2025-02-03 13:33:55 -0500

    fini

--- Assets/DetectionCible3D.cs
+++ Assets/DetectionCible3D.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectionCible3D : MonoBehaviour
+
 {
-    // Start is called before the first frame update
-    void Start()
+
+    void OnTriggerEnter(Collider collision)
     {
-        
+        print(collision.gameObject.name + " : " + collision.gameObject.tag);
+     
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+
+        if (collision.gameObject.CompareTag("Cible"))
+        {
+            print(collision.gameObject.name + " : " + collision.gameObject.tag);
+            Destroy(collision.gameObject);
+        }
+
     }
 }
